1a6f7afa7b3d3decbfc5e1a4c074efead4976268
scionsamurai <scionsamurai@yahoo.com>
2024-04-28 19:14:01 -0500

    updating actions

---
+++ .github/workflows/check_version.py
@@ -0,0 +1,24 @@
+import os
+
+# Get the path to the directory containing the script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the Cargo.toml file
+cargo_toml_path = os.path.join(script_dir, '..', '..', 'Cargo.toml')
+
+# Read the current Cargo.toml file
+with open(cargo_toml_path, 'r') as f:
+    current_contents = f.read()
+
+# Read the Cargo.toml file from the previous commit
+previous_contents = os.popen('git show HEAD~1:' + cargo_toml_path).read()
+
+# Extract the version numbers from the files
+current_version = current_contents.split('version = "')[1].split('"')[0]
+previous_version = previous_contents.split('version = "')[1].split('"')[0]
+
+# Compare the version numbers
+version_changed = current_version != previous_version
+
+# Output the result
+print(f"::set-output name=version_changed::{version_changed}")
