cb0d675f9545d0ea753a1711a9e4fac56474e00b
scionsamurai <scionsamurai@yahoo.com>
2024-04-28 19:28:47 -0500

    updating actions

--- check_version.py
+++ check_version.py
@@ -10,15 +10,16 @@ cargo_toml_path = os.path.join(current_dir, 'Cargo.toml')
 with open(cargo_toml_path, 'r') as f:
     current_contents = f.read()
 
-# Check out the previous commit temporarily
-os.system('git checkout HEAD~1')
+# Get the path to the previous version of the Cargo.toml file
+previous_cargo_toml_path = os.path.join(current_dir, '..', 'Cargo.toml')
 
-# Read the Cargo.toml file from the previous commit
-with open(cargo_toml_path, 'r') as f:
-    previous_contents = f.read()
-
-# Check out the current commit again
-os.system('git checkout -')
+# Read the Cargo.toml file from the previous commit (if available)
+if os.path.exists(previous_cargo_toml_path):
+    with open(previous_cargo_toml_path, 'r') as f:
+        previous_contents = f.read()
+else:
+    # If there's no previous version of the file, set previous_contents to an empty string
+    previous_contents = ''
 
 # Extract the version numbers from the files
 current_version = current_contents.split('version = "')[1].split('"')[0]
